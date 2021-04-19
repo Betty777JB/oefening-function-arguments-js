26abbf133af03a22bef4da2674d12bdf7d320b06
Betty Bialkowska <betty_1980_live@outlook.com>
2021-04-20 00:14:33 +0200

    einde oefening-functions-arguments-js

---
+++ script.js
@@ -0,0 +1,15 @@
+/*function redWallPaitner(colors) {
+console.log('The wall has been painted '  + colors); //...string painted spatie dan '
+
+
+
+
+}
+
+redWallPaitner('green');
+redWallPaitner('yellow');// without argument console undefined
+*/
+function myWall(idWall1, idWall2) {
+console.log("The north wall has been painted orange " + "The south-east wall has been painted grey");
+}
+myWall(idWall1,idWall2);
